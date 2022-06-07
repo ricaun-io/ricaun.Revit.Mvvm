1761239ea8a6776ad0daf8bcd4cf77a8359fdf98
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-06-06 22:56:27 -0300

    AsyncRelayCommand

--- ricaun.Revit.Mvvm/ComponentModel/ObservableObject.cs
+++ ricaun.Revit.Mvvm/ComponentModel/ObservableObject.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace ricaun.Revit.Mvvm
 {
@@ -33,5 +36,285 @@ namespace ricaun.Revit.Mvvm
                 RefreshProperty(property.Name);
             }
         }
+
+        #region Changes
+
+        /// <summary>
+        /// Compares the current and new values for a given field (which should be the backing
+        /// field for a property). If the value has changed, raises the <see cref="PropertyChanging"/>
+        /// event, updates the field and then raises the <see cref="PropertyChanged"/> event.
+        /// The behavior mirrors that of <see cref="SetProperty{T}(ref T,T,string)"/>, with the difference being that
+        /// this method will also monitor the new value of the property (a generic <see cref="Task"/>) and will also
+        /// raise the <see cref="PropertyChanged"/> again for the target property when it completes.
+        /// This can be used to update bindings observing that <see cref="Task"/> or any of its properties.
+        /// This method and its overload specifically rely on the <see cref="TaskNotifier"/> type, which needs
+        /// to be used in the backing field for the target <see cref="Task"/> property. The field doesn't need to be
+        /// initialized, as this method will take care of doing that automatically. The <see cref="TaskNotifier"/>
+        /// type also includes an implicit operator, so it can be assigned to any <see cref="Task"/> instance directly.
+        /// Here is a sample property declaration using this method:
+        /// <code>
+        /// private TaskNotifier myTask;
+        ///
+        /// public Task MyTask
+        /// {
+        ///     get => myTask;
+        ///     private set => SetAndNotifyOnCompletion(ref myTask, value);
+        /// }
+        /// </code>
+        /// </summary>
+        /// <param name="taskNotifier">The field notifier to modify.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        /// <remarks>
+        /// The <see cref="PropertyChanging"/> and <see cref="PropertyChanged"/> events are not raised if the current
+        /// and new value for the target property are the same. The return value being <see langword="true"/> only
+        /// indicates that the new value being assigned to <paramref name="taskNotifier"/> is different than the previous one,
+        /// and it does not mean the new <see cref="Task"/> instance passed as argument is in any particular state.
+        /// </remarks>
+        protected bool SetPropertyAndNotifyOnCompletion(ref TaskNotifier taskNotifier, Task newValue, [CallerMemberName] string propertyName = null)
+        {
+            // We invoke the overload with a callback here to avoid code duplication, and simply pass an empty callback.
+            // The lambda expression here is transformed by the C# compiler into an empty closure class with a
+            // static singleton field containing a closure instance, and another caching the instantiated Action<TTask>
+            // instance. This will result in no further allocations after the first time this method is called for a given
+            // generic type. We only pay the cost of the virtual call to the delegate, but this is not performance critical
+            // code and that overhead would still be much lower than the rest of the method anyway, so that's fine.
+            return SetPropertyAndNotifyOnCompletion(taskNotifier ??= new(), newValue, static _ => { }, propertyName);
+        }
+
+        /// <summary>
+        /// Compares the current and new values for a given field (which should be the backing
+        /// field for a property). If the value has changed, raises the <see cref="PropertyChanging"/>
+        /// event, updates the field and then raises the <see cref="PropertyChanged"/> event.
+        /// This method is just like <see cref="SetPropertyAndNotifyOnCompletion(ref TaskNotifier,Task,string)"/>,
+        /// with the difference being an extra <see cref="Action{T}"/> parameter with a callback being invoked
+        /// either immediately, if the new task has already completed or is <see langword="null"/>, or upon completion.
+        /// </summary>
+        /// <param name="taskNotifier">The field notifier to modify.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        /// <param name="callback">A callback to invoke to update the property value.</param>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        /// <remarks>
+        /// The <see cref="PropertyChanging"/> and <see cref="PropertyChanged"/> events are not raised
+        /// if the current and new value for the target property are the same.
+        /// </remarks>
+        protected bool SetPropertyAndNotifyOnCompletion(ref TaskNotifier taskNotifier, Task newValue, Action<Task> callback, [CallerMemberName] string propertyName = null)
+        {
+            return SetPropertyAndNotifyOnCompletion(taskNotifier ??= new(), newValue, callback, propertyName);
+        }
+
+        /// <summary>
+        /// Compares the current and new values for a given field (which should be the backing
+        /// field for a property). If the value has changed, raises the <see cref="PropertyChanging"/>
+        /// event, updates the field and then raises the <see cref="PropertyChanged"/> event.
+        /// The behavior mirrors that of <see cref="SetProperty{T}(ref T,T,string)"/>, with the difference being that
+        /// this method will also monitor the new value of the property (a generic <see cref="Task"/>) and will also
+        /// raise the <see cref="PropertyChanged"/> again for the target property when it completes.
+        /// This can be used to update bindings observing that <see cref="Task"/> or any of its properties.
+        /// This method and its overload specifically rely on the <see cref="TaskNotifier{T}"/> type, which needs
+        /// to be used in the backing field for the target <see cref="Task"/> property. The field doesn't need to be
+        /// initialized, as this method will take care of doing that automatically. The <see cref="TaskNotifier{T}"/>
+        /// type also includes an implicit operator, so it can be assigned to any <see cref="Task"/> instance directly.
+        /// Here is a sample property declaration using this method:
+        /// <code>
+        /// private TaskNotifier&lt;int&gt; myTask;
+        ///
+        /// public Task&lt;int&gt; MyTask
+        /// {
+        ///     get => myTask;
+        ///     private set => SetAndNotifyOnCompletion(ref myTask, value);
+        /// }
+        /// </code>
+        /// </summary>
+        /// <typeparam name="T">The type of result for the <see cref="Task{TResult}"/> to set and monitor.</typeparam>
+        /// <param name="taskNotifier">The field notifier to modify.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        /// <remarks>
+        /// The <see cref="PropertyChanging"/> and <see cref="PropertyChanged"/> events are not raised if the current
+        /// and new value for the target property are the same. The return value being <see langword="true"/> only
+        /// indicates that the new value being assigned to <paramref name="taskNotifier"/> is different than the previous one,
+        /// and it does not mean the new <see cref="Task{TResult}"/> instance passed as argument is in any particular state.
+        /// </remarks>
+        protected bool SetPropertyAndNotifyOnCompletion<T>(ref TaskNotifier<T> taskNotifier, Task<T> newValue, [CallerMemberName] string propertyName = null)
+        {
+            return SetPropertyAndNotifyOnCompletion(taskNotifier ??= new(), newValue, static _ => { }, propertyName);
+        }
+
+        /// <summary>
+        /// Compares the current and new values for a given field (which should be the backing
+        /// field for a property). If the value has changed, raises the <see cref="PropertyChanging"/>
+        /// event, updates the field and then raises the <see cref="PropertyChanged"/> event.
+        /// This method is just like <see cref="SetPropertyAndNotifyOnCompletion{T}(ref TaskNotifier{T},Task{T},string)"/>,
+        /// with the difference being an extra <see cref="Action{T}"/> parameter with a callback being invoked
+        /// either immediately, if the new task has already completed or is <see langword="null"/>, or upon completion.
+        /// </summary>
+        /// <typeparam name="T">The type of result for the <see cref="Task{TResult}"/> to set and monitor.</typeparam>
+        /// <param name="taskNotifier">The field notifier to modify.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        /// <param name="callback">A callback to invoke to update the property value.</param>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        /// <remarks>
+        /// The <see cref="PropertyChanging"/> and <see cref="PropertyChanged"/> events are not raised
+        /// if the current and new value for the target property are the same.
+        /// </remarks>
+        protected bool SetPropertyAndNotifyOnCompletion<T>(ref TaskNotifier<T> taskNotifier, Task<T> newValue, Action<Task<T>> callback, [CallerMemberName] string propertyName = null)
+        {
+            return SetPropertyAndNotifyOnCompletion(taskNotifier ??= new(), newValue, callback, propertyName);
+        }
+
+        /// <summary>
+        /// Implements the notification logic for the related methods.
+        /// </summary>
+        /// <typeparam name="TTask">The type of <see cref="Task"/> to set and monitor.</typeparam>
+        /// <param name="taskNotifier">The field notifier.</param>
+        /// <param name="newValue">The property's value after the change occurred.</param>
+        /// <param name="callback">A callback to invoke to update the property value.</param>
+        /// <param name="propertyName">(optional) The name of the property that changed.</param>
+        /// <returns><see langword="true"/> if the property was changed, <see langword="false"/> otherwise.</returns>
+        private bool SetPropertyAndNotifyOnCompletion<TTask>(ITaskNotifier<TTask> taskNotifier, TTask newValue, Action<TTask> callback, [CallerMemberName] string propertyName = null)
+            where TTask : Task
+        {
+            if (ReferenceEquals(taskNotifier.Task, newValue))
+            {
+                return false;
+            }
+
+            // Check the status of the new task before assigning it to the
+            // target field. This is so that in case the task is either
+            // null or already completed, we can avoid the overhead of
+            // scheduling the method to monitor its completion.
+            bool isAlreadyCompletedOrNull = newValue?.IsCompleted ?? true;
+
+            taskNotifier.Task = newValue;
+
+            RefreshProperty(propertyName);
+
+            // If the input task is either null or already completed, we don't need to
+            // execute the additional logic to monitor its completion, so we can just bypass
+            // the rest of the method and return that the field changed here. The return value
+            // does not indicate that the task itself has completed, but just that the property
+            // value itself has changed (ie. the referenced task instance has changed).
+            // This mirrors the return value of all the other synchronous Set methods as well.
+            if (isAlreadyCompletedOrNull)
+            {
+                callback(newValue);
+
+                return true;
+            }
+
+            // We use a local async function here so that the main method can
+            // remain synchronous and return a value that can be immediately
+            // used by the caller. This mirrors Set<T>(ref T, T, string).
+            // We use an async void function instead of a Task-returning function
+            // so that if a binding update caused by the property change notification
+            // causes a crash, it is immediately reported in the application instead of
+            // the exception being ignored (as the returned task wouldn't be awaited),
+            // which would result in a confusing behavior for users.
+            async void MonitorTask()
+            {
+                try
+                {
+                    // Await the task and ignore any exceptions
+                    await newValue!;
+                }
+                catch
+                {
+                }
+
+                // Only notify if the property hasn't changed
+                if (ReferenceEquals(taskNotifier.Task, newValue))
+                {
+                    RefreshProperty(propertyName);
+                }
+
+                callback(newValue);
+            }
+
+            MonitorTask();
+
+            return true;
+        }
+
+        /// <summary>
+        /// An interface for task notifiers of a specified type.
+        /// </summary>
+        /// <typeparam name="TTask">The type of value to store.</typeparam>
+        private interface ITaskNotifier<TTask>
+            where TTask : Task
+        {
+            /// <summary>
+            /// Gets or sets the wrapped <typeparamref name="TTask"/> value.
+            /// </summary>
+            TTask Task { get; set; }
+        }
+
+        /// <summary>
+        /// A wrapping class that can hold a <see cref="Task"/> value.
+        /// </summary>
+        protected sealed class TaskNotifier : ITaskNotifier<Task>
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TaskNotifier"/> class.
+            /// </summary>
+            internal TaskNotifier()
+            {
+            }
+
+            private Task task;
+
+            /// <inheritdoc/>
+            Task ITaskNotifier<Task>.Task
+            {
+                get => this.task;
+                set => this.task = value;
+            }
+
+            /// <summary>
+            /// Unwraps the <see cref="Task"/> value stored in the current instance.
+            /// </summary>
+            /// <param name="notifier">The input <see cref="TaskNotifier{TTask}"/> instance.</param>
+            public static implicit operator Task(TaskNotifier notifier)
+            {
+                return notifier?.task;
+            }
+        }
+
+        /// <summary>
+        /// A wrapping class that can hold a <see cref="Task{T}"/> value.
+        /// </summary>
+        /// <typeparam name="T">The type of value for the wrapped <see cref="Task{T}"/> instance.</typeparam>
+        protected sealed class TaskNotifier<T> : ITaskNotifier<Task<T>>
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TaskNotifier{TTask}"/> class.
+            /// </summary>
+            internal TaskNotifier()
+            {
+            }
+
+            private Task<T> task;
+
+            /// <inheritdoc/>
+            Task<T> ITaskNotifier<Task<T>>.Task
+            {
+                get => this.task;
+                set => this.task = value;
+            }
+
+            /// <summary>
+            /// Unwraps the <see cref="Task{T}"/> value stored in the current instance.
+            /// </summary>
+            /// <param name="notifier">The input <see cref="TaskNotifier{TTask}"/> instance.</param>
+            public static implicit operator Task<T>(TaskNotifier<T> notifier)
+            {
+                return notifier?.task;
+            }
+        }
     }
+    #endregion
 }
---
+++ ricaun.Revit.Mvvm/Input/AsyncRelayCommand.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ricaun.Revit.Mvvm
+{
+    /// <summary>
+    /// An interface expanding <see cref="IRelayCommand"/> to support asynchronous operations.
+    /// </summary>
+    public interface IAsyncRelayCommand : IRelayCommand
+    {
+
+    }
+
+    /// <summary>
+    /// A command that mirrors the functionality of <see cref="RelayCommand"/>, with the addition of
+    /// accepting a <see cref="Func{TResult}"/> returning a <see cref="Task"/> as the execute
+    /// action, and providing an <see cref="ExecutionTask"/> property that notifies changes when
+    /// <see cref="ExecuteAsync"/> is invoked and when the returned <see cref="Task"/> completes.
+    /// </summary>
+    public sealed class AsyncRelayCommand : ObservableObject, IAsyncRelayCommand
+    {
+        /// <summary>
+        /// The <see cref="Func{TResult}"/> to invoke when <see cref="Execute"/> is used.
+        /// </summary>
+        private readonly Func<Task> execute;
+
+        /// <summary>
+        /// The cancelable <see cref="Func{T,TResult}"/> to invoke when <see cref="Execute"/> is used.
+        /// </summary>
+        /// <remarks>Only one between this and <see cref="execute"/> is not <see langword="null"/>.</remarks>
+        private readonly Func<CancellationToken, Task> cancelableExecute;
+
+        /// <summary>
+        /// The optional action to invoke when <see cref="CanExecute"/> is used.
+        /// </summary>
+        private readonly Func<bool> canExecute;
+
+        /// <summary>
+        /// The <see cref="CancellationTokenSource"/> instance to use to cancel <see cref="cancelableExecute"/>.
+        /// </summary>
+        /// <remarks>This is only used when <see cref="cancelableExecute"/> is not <see langword="null"/>.</remarks>
+        private CancellationTokenSource cancellationTokenSource;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncRelayCommand"/> class that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public AsyncRelayCommand(Func<Task> execute)
+        {
+            this.execute = execute;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncRelayCommand"/> class that can always execute.
+        /// </summary>
+        /// <param name="cancelableExecute">The cancelable execution logic.</param>
+        public AsyncRelayCommand(Func<CancellationToken, Task> cancelableExecute)
+        {
+            this.cancelableExecute = cancelableExecute;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncRelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncRelayCommand"/> class.
+        /// </summary>
+        /// <param name="cancelableExecute">The cancelable execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public AsyncRelayCommand(Func<CancellationToken, Task> cancelableExecute, Func<bool> canExecute)
+        {
+            this.cancelableExecute = cancelableExecute;
+            this.canExecute = canExecute;
+        }
+
+        private TaskNotifier executionTask;
+
+        /// <inheritdoc/>
+        public Task ExecutionTask
+        {
+            get => this.executionTask;
+            private set
+            {
+                if (SetPropertyAndNotifyOnCompletion(ref this.executionTask, value, _ =>
+                {
+
+                }))
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// CanExecuteChanged
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute?.Invoke() != false;
+        }
+
+        public void Execute(object parameter)
+        {
+            _ = ExecuteAsync(parameter);
+        }
+
+        public Task ExecuteAsync(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                // Non cancelable command delegate
+                if (this.execute is not null)
+                {
+                    return ExecutionTask = this.execute();
+                }
+
+                // Cancel the previous operation, if one is pending
+                this.cancellationTokenSource?.Cancel();
+
+                CancellationTokenSource cancellationTokenSource = this.cancellationTokenSource = new();
+
+                // Invoke the cancelable command delegate with a new linked token
+                return ExecutionTask = this.cancelableExecute!(cancellationTokenSource.Token);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
